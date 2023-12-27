d49d97300c35f6f65323418d1eb8ccecec49dbf2
The Luke <100490693+Luki013@users.noreply.github.com>
2023-12-27 20:14:02 -0300

    Removed Files

--- engine/Core/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/arauj/OneDrive/Documentos/Kingdom Engine/engine/Core
-# Build directory: C:/Users/arauj/OneDrive/Documentos/Kingdom Engine/engine/Core/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
