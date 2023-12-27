67bc430acfa4968559f717930bd935a81784cbfb
The Luke <100490693+Luki013@users.noreply.github.com>
2023-12-27 20:08:40 -0300

    Changed project: Added CMake

---
+++ engine/Core/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/arauj/OneDrive/Documentos/Kingdom Engine/engine/Core
+# Build directory: C:/Users/arauj/OneDrive/Documentos/Kingdom Engine/engine/Core/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
